0e16fee2a1f296ea64cb325aae3990b3146bd5a5
Rostom Turki <rostomtourki04@gmail.com>
2026-09-21 09:23:57 +0200

    refactor: simplify fibonacci function logic

--- notebook.py
+++ notebook.py
@@ -14,10 +14,8 @@ def _():
 
 @app.function
 def fibonacci(n: int) -> int:
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n <= 1:
+        return n
     return fibonacci(n-1) + fibonacci(n-2)
 
 
